28e57068efef0d0a975eabf8c2614ecdec1757a4
Luis Macena <luis.macena@outlook.com>
2022-10-21 19:27:24 -0300

    apostila08(react-hooks) secao 2.7

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
-
+import './components/Accordion.css'
 ReactDOM.render(
   <App />,
   document.querySelector('#root')
